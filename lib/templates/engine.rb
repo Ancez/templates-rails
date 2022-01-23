cf44adbf957a3c75a8fb98db158dd34fe33e7900
ancez <Lukasz.czapiewski@mmtm.io>
2022-01-23 16:09:50 +0000

    add separate assets precompile

--- lib/templates/engine.rb
+++ lib/templates/engine.rb
@@ -1,5 +1,9 @@
 module Templates
   class Engine < ::Rails::Engine
     isolate_namespace Templates
+
+    initializer 'templates.assets.precompile' do |app|
+      app.config.assets.precompile += %w( application.css )
+    end
   end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  mount Templates::Engine, at: '/'
+  mount Templates::Engine => '/'
 end
