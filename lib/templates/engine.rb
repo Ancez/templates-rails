245a40e785770b31bf2fca7c1cc34c06181567a3
ancez <Lukasz.czapiewski@mmtm.io>
2022-03-06 18:23:04 +0000

    update readme and add rails env dev fallback on auto mount

--- lib/templates/engine.rb
+++ lib/templates/engine.rb
@@ -2,7 +2,7 @@ module Templates
   class Engine < ::Rails::Engine
     initializer 'templates', before: :load_config_initializers do
       Rails.application.routes.append do
-        mount Templates::Engine => '/templates'
+        mount Templates::Engine => '/templates' if Rails.env.development?
       end
     end
 
