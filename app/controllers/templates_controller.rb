188d04868c65b2a74b0cadbfd638aa71414aebf9
Lukasz Czapiewski <lukasz.czapiewski2@gmail.com>
2023-07-14 20:38:43 +0100

    add controller inheritance

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,4 +1,4 @@
-class TemplatesController < ActionController::Base
+class TemplatesController < Templates.parent_controller.constantize
   def index
     render :index, layout: 'templates/index'
   end
--- lib/templates-rails.rb
+++ lib/templates-rails.rb
@@ -2,5 +2,9 @@ require 'templates/version'
 require 'templates/engine'
 
 module Templates
-  # Your code goes here...
+  # The parent controller for TemplatesController to inherit from.
+  # This is set early in the initialization process and should be set to a string.
+  # It needs to be a string so the parent controller reloads whenever there are changes in development.
+  mattr_accessor :parent_controller
+  @@parent_controller = 'ApplicationController'
 end
